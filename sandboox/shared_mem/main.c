26f6b40e8efa95fc0fdf6314c55a26b7eb041a97
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-04-08 12:52:38 -0300

    Shared memory - read
    Initial commit of reading shared memory

--- sandboox/shared_mem/main.c
+++ sandboox/shared_mem/main.c
@@ -37,17 +37,26 @@ int main ( int argc, char *argv[] )
 {
 	int pid = fork();
 	int shared_memory;
-	if(pid){
-
+	char * hello = "Hello";
+	if(pid){ //father proccess
 		shared_memory = shm_open("/myshm", O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
 		void * addr = mmap( NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_memory, 0);
 		if( addr == MAP_FAILED){
 			perror("Map failed");
 		}
-		char * hello = "Hello";
 		ftruncate(shared_memory,strlen(hello));
 		memcpy(addr, hello, strlen(hello));
-	}else{
+	}else{ //child proccess
+		sleep(3);
+		shared_memory = shm_open("/myshm", O_RDONLY, S_IRUSR | S_IWUSR);
+		void * addr = mmap( NULL, SIZE, PROT_READ , MAP_SHARED, shared_memory, 0);
+		if( addr == MAP_FAILED){
+			perror("Child proccess map failed");
+		}
+		char * str_read = (char*) malloc(strlen(hello) + 1);
+		str_read[strlen(hello)] = '\0';
+		memcpy(str_read,addr, strlen(hello));
+		printf("Second proccess read %s\n", str_read);
 	}
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
