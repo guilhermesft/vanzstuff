9ca580a9d795b8ce8d2096ab3bdba612dd7f6468
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-04-22 20:02:32 -0300

    POSIX Shared memory with struct
    The POSIX shared memory sample is using a struct as a shared memory
    segment

--- ipc/shmposix.c
+++ ipc/shmposix.c
@@ -3,7 +3,10 @@
  *
  *       Filename:  shmposix.c
  *
- *    Description:
+ *    Description: This source implements a simple sample of POSIX standart shared memory
+ *    		   This semple does not use semaphores. Semaphores semple is will implemented
+ *    		   This sample also has not a lot of good practices. This sample is just for
+ *		   fun. ;)
  *
  *        Version:  1.0
  *        Created:  04/15/2014 12:11:32 AM
@@ -23,8 +26,17 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/fcntl.h>
+#include <sys/wait.h>
 
 #define SIZE getpagesize()
+#define SHM_NAME "/vanz"
+
+struct msg{
+	char * msg;
+};
+
+void executeParent();
+void executeChild();
 
 /*
  * ===  FUNCTION  ======================================================================
@@ -34,23 +46,60 @@
  */
 int main ( int argc, char *argv[] )
 {
-	int shmfd = shm_open("/vanz", O_CREAT | O_RDWR, S_IRWXU);
+	int pid = fork();
+	if( pid ){
+		executeParent();
+		printf("Parent waiting...\n");
+		int status = 0;
+		wait(&status); //wait for child process
+		printf("Parent done\n");
+	}else{
+		executeChild();
+		printf("Child done!\n");
+	}
+	return EXIT_SUCCESS;
+}
+
+void executeParent()
+{
+	int shmfd = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRWXU);
 	if(shmfd < 0){
-		perror("shm_open failed");
+		perror("[Parent] shm_open failed");
 		exit(1);
 	}
-	char * data = (char*) mmap(NULL,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0 );
+	struct msg * data = (struct msg*) mmap(NULL,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0 );
 	if( data == MAP_FAILED ){
-		perror("SHM map failed: ");
+		perror("[Parent] SHM map failed: ");
 		exit(1);
 	}
 	if( ftruncate(shmfd, SIZE) == -1 ){
-		perror("ftruncate failed");
+		perror("[Parent] ftruncate failed");
 		exit(1);
 	}
-	char * msg = "Hi, Child!";
-	strncpy(data, msg, SIZE);
+	data->msg = "Hi, Child!";
+	printf("Parent wrote = %s\n", data->msg);
+	sleep(3);
+	printf("Parent read = %s\n", data->msg);
 	munmap(data, SIZE);
 	close(shmfd);
-	return EXIT_SUCCESS;
-}				/* ----------  end of function main  ---------- */
+}
+
+void executeChild()
+{
+	sleep(1);
+	int shmfd = shm_open(SHM_NAME, O_RDWR, S_IRWXU);
+	if(shmfd < 0){
+		perror("[Child] shm_open failed");
+		exit(1);
+	}
+	struct msg * data = (struct msg*) mmap(NULL,SIZE, PROT_WRITE | PROT_READ , MAP_SHARED, shmfd, 0 );
+	if( data == MAP_FAILED ){
+		perror("[Child] SHM map failed: ");
+		exit(1);
+	}
+	printf("Child read = %s\n", data->msg);
+	data->msg="Hello, father!";
+	printf("Child wrote = %s\n", data->msg);
+	munmap(data, SIZE);
+	close(shmfd);
+}
