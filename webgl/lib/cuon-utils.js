6e8da94f6c0fc9f7b880857f0dc203792c4ce7f0
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-09-30 23:12:40 -0300

    WebGL sample
    Adding a WebGL sample program where was drawed a simple red point using shaders

--- webgl/lib/cuon-utils.js
+++ webgl/lib/cuon-utils.js
@@ -6,3 +6,50 @@ function getWebGLContext( canvas, debug){
 	}
 	return ctx;
 }
+
+function initShaders(gl, vshader, fshader){
+	var vertexShader = compile(gl, gl.VERTEX_SHADER, vshader);
+	var fragmentShader = compile(gl, gl.FRAGMENT_SHADER, fshader);
+	var program = null;
+	if( vertexShader && fragmentShader ){
+		program = link(gl, vertexShader, fragmentShader);
+	}
+	if( !program ){
+		return false;
+	}
+	gl.useProgram(program);
+	gl.program = program;
+	return true;
+}
+
+/**
+ * This function link and return a program that uses the vertex and fragment
+ * shader passed in the parameteres
+ */
+function link(gl, vertexShader, fragmentShader){
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+
+	if( !gl.getProgramParameter(program, gl.LINK_STATUS)){
+		throw new Error(gl.getProgramInfoLog(program));
+	}
+	return program;
+};
+
+/**
+ * Compile the shader source and return the shader obj
+ */
+function compile(gl, type, shaderSrc){
+	var shader = gl.createShader(type);
+	gl.shaderSource(shader, shaderSrc);
+	gl.compileShader(shader);
+
+	if( !gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+		throw new Error(gl.getShaderInfoLog(shader));
+	}
+	return shader;
+};
+
+
