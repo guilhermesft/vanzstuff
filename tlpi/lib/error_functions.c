d2e42db7ad4a2af179bebf73fd2f5a0bfc2d5a09
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-08-20 00:02:15 -0300

    lib: makefile and changed lib files
    - created a makefile to compile all source files
    - the libs files changed to be conform with the book's ones

--- tlpi/lib/error_functions.c
+++ tlpi/lib/error_functions.c
@@ -1,7 +1,17 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2015.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Lesser General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+\*************************************************************************/
+
 #include <stdarg.h>
 #include "error_functions.h"
 #include "tlpi_hdr.h"
-#include "ename.c.inc"
+#include "ename.c.inc"          /* Defines ename and MAX_ENAME */
 
 #ifdef __GNUC__
 __attribute__ ((__noreturn__))
@@ -9,110 +19,138 @@ __attribute__ ((__noreturn__))
 static void
 terminate(Boolean useExit3)
 {
-	char *s;
-	s = getenv("EF_DUMPCORE");
-	if( s != NULL && *s != '\0')
-		abort();
-	else if(useExit3)
-		exit(EXIT_FAILURE);
-	else
-		_exit(EXIT_FAILURE);
+    char *s;
+
+    /* Dump core if EF_DUMPCORE environment variable is defined and
+       is a nonempty string; otherwise call exit(3) or _exit(2),
+       depending on the value of 'useExit3'. */
+
+    s = getenv("EF_DUMPCORE");
+
+    if (s != NULL && *s != '\0')
+        abort();
+    else if (useExit3)
+        exit(EXIT_FAILURE);
+    else
+        _exit(EXIT_FAILURE);
 }
 
 static void
-outputError(Boolean useErr, int err, Boolean flushStdout, const char *format, va_list ap)
+outputError(Boolean useErr, int err, Boolean flushStdout,
+        const char *format, va_list ap)
 {
 #define BUF_SIZE 500
-	char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
+    char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
 
-	vsnprintf(userMsg, BUF_SIZE, format, ap);
-	if( userErr)
-		snprintf(errText, BUF_SIZE, " [%s %s]", (err > 0 && err <= MAX_ENAME) ? ename[err] : "?UNKNOWN?", strerror(err));
-	else
-		snprintf(errTextm BUF_SIZE, ":");
+    vsnprintf(userMsg, BUF_SIZE, format, ap);
 
-	snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
-	if( flushStdout)
-		fflush(stdout);
-	fputs(buf, stderr);
-	fflush(stderr);
+    if (useErr)
+        snprintf(errText, BUF_SIZE, " [%s %s]",
+                (err > 0 && err <= MAX_ENAME) ?
+                ename[err] : "?UNKNOWN?", strerror(err));
+    else
+        snprintf(errText, BUF_SIZE, ":");
+
+    snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
+
+    if (flushStdout)
+        fflush(stdout);       /* Flush any pending stdout */
+    fputs(buf, stderr);
+    fflush(stderr);           /* In case stderr is not line-buffered */
 }
 
 void
 errMsg(const char *format, ...)
 {
-	va_list argList;
-	int savedErrno;
+    va_list argList;
+    int savedErrno;
 
-	savedErrno = errno;
-	va_start(argList, format);
-	outputError(TRUE, errno, TRUE, format, argList);
-	va_end(argList);
-	errno = savedErrno;
+    savedErrno = errno;       /* In case we change it here */
+
+    va_start(argList, format);
+    outputError(TRUE, errno, TRUE, format, argList);
+    va_end(argList);
+
+    errno = savedErrno;
 }
 
 void
 errExit(const char *format, ...)
 {
-	va_list argList;
-	va_start(argList, format);
-	outputError(TRUE, errno, TRUE, format, argList);
-	va_end(argList);
-	terminate(TRUE);
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errno, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
 }
 
 void
 err_exit(const char *format, ...)
 {
-	va_list argList;
-	va_start(argList, format);
-	outputError(TRUE, errno, FALSE, format, argList);
-	va_end(argList);
-	terminate(FALSE);
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errno, FALSE, format, argList);
+    va_end(argList);
+
+    terminate(FALSE);
 }
 
-void errExitEN(int errnum, const char *format, ...)
+void
+errExitEN(int errnum, const char *format, ...)
 {
-	va_list argList;
-	va_start(argList, format);
-	outputError(TRUE, errnum, TRUE, format, argList);
-	va_end(argList);
-	terminate(TRUE);
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errnum, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
 }
 
 void
 fatal(const char *format, ...)
 {
-	va_list argList;
-	va_start(argList, format);
-	outputError(FALSE, 0, TRUE, format, argList);
-	va_end(argList);
-	terminate(TRUE);A
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(FALSE, 0, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
 }
 
 void
 usageErr(const char *format, ...)
 {
-	va_list argList;
-	fflush(stdout);
-	fprintf(stderr, "Usage: ");
-	va_start(argList, format);
-	vfprintf(stderr, format, argList);
-	va_end(argList);
-	fflush(stderr);
-	exit(EXIT_FAILURE);
+    va_list argList;
+
+    fflush(stdout);           /* Flush any pending stdout */
+
+    fprintf(stderr, "Usage: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);           /* In case stderr is not line-buffered */
+    exit(EXIT_FAILURE);
 }
 
 void
 cmdLineErr(const char *format, ...)
 {
-	va_list argList;
-	fflush(stdout);
-	fprintf(stderr, "Command-line usage error: ");
-	va_start(argList, format);
-	vfprintf(stderr, format, argList);
-	va_end(argList);
+    va_list argList;
 
-	fflush(stderr);
-	exit(EXIT_FAILURE);
+    fflush(stdout);           /* Flush any pending stdout */
+
+    fprintf(stderr, "Command-line usage error: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);           /* In case stderr is not line-buffered */
+    exit(EXIT_FAILURE);
 }
+
--- tlpi/lib/get_num.h
+++ tlpi/lib/get_num.h
@@ -1,12 +1,12 @@
 /*************************************************************************\
- * *                  Copyright (C) Michael Kerrisk, 2015.                   *
- * *                                                                         *
- * * This program is free software. You may use, modify, and redistribute it *
- * * under the terms of the GNU Lesser General Public License as published   *
- * * by the Free Software Foundation, either version 3 or (at your option)   *
- * * any later version. This program is distributed without any warranty.    *
- * * See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
- * \*************************************************************************/
+*                  Copyright (C) Michael Kerrisk, 2015.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Lesser General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+\*************************************************************************/
 
 #ifndef GET_NUM_H
 #define GET_NUM_H
@@ -24,4 +24,3 @@ long getLong(const char *arg, int flags, const char *name);
 int getInt(const char *arg, int flags, const char *name);
 
 #endif
-
