84bb8f727e7fa163238f65bbc55c19da6a9d25ea
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-08-24 01:54:51 -0300

    memalloc: implement malloc and free
    Added the initial files to implement malloc and free functions

---
+++ tlpi/memalloc/exec/memalloc.c
@@ -0,0 +1,14 @@
+
+#include "memalloc.h"
+
+void* valloc(size_t bytes)
+{
+	//TODO
+	return NULL;
+}
+
+int vfree(void* ptr)
+{
+	//TODO
+	return -1;
+}
---
+++ tlpi/memalloc/exec/memalloc.h
@@ -0,0 +1,18 @@
+#ifndef _MEMALLOC_HEADER
+#define  _MEMALLOC_HEADER
+
+#include <unistd.h>
+
+/**
+ * Allocates a block of memory with bytes of length
+ * @returns a pointer to the allocated block of memory. Return NULL in error
+ */
+void* valloc(size_t bytes);
+
+/**
+ * Deallocates the ptr block of memory
+ * @returns 0 on success
+ */
+int vfree(void* ptr);
+
+#endif
