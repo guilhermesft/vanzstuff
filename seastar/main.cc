bb0415e94d936c2e18bde65e390b0df0e95df6cf
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-10-31 22:47:55 -0200

    seastar demo
    Adds more simple code to play with the seastar framework
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- seastar/main.cc
+++ seastar/main.cc
@@ -1,11 +1,37 @@
 #include "core/app-template.hh"
-#include "core/reactor.hh"
+#include "core/future.hh"
+#include "core/sleep.hh"
+#include "util/log.hh"
+
 #include <iostream>
+#include <stdexcept>
+
+seastar::future<int> fast()
+{
+	return seastar::make_ready_future<int>(3);
+}
+
+seastar::future<int> incr(int i)
+{
+	using namespace std::chrono_literals;
+	return seastar::sleep(10ms).then([i] { return i + 1; });
+}
+
+extern seastar::future<> f() 
+{
+	return incr(3).then([] (int val) {
+		std::cout << "Got " << val << std::endl;
+	});
+}
 
 int main(int argc, char** argv) {
 	seastar::app_template app;
-	app.run(argc, argv, [] {
-	std::cout << "Hello world\n";
-		return seastar::make_ready_future<>();
-	});
+	try {
+		app.run(argc, argv, f);
+	} catch (...) {
+		std::cerr << "Couldn't start application: "
+			<< std::current_exception() << std::endl;
+		return 1;
+	}
+	return 0;
 }
