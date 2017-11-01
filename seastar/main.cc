20b7f5a1b51df3822c8b297ba8ccf503d5c8a05e
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-10-31 23:55:49 -0200

    seastar: run function in multiple cores
    Adds a new code snipped showing how to run a function in a remote core
    using the seastar framework
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- seastar/main.cc
+++ seastar/main.cc
@@ -1,26 +1,50 @@
 #include "core/app-template.hh"
+#include "core/future-util.hh"
 #include "core/future.hh"
+#include "core/reactor.hh"
+#include "core/seastar.hh"
 #include "core/sleep.hh"
 #include "util/log.hh"
 
 #include <iostream>
 #include <stdexcept>
+	
+using namespace std::chrono_literals;
+
+// prototypes
+seastar::future<> service_loop();
+seastar::future<> slow_op();
+seastar::future<int> fast();
+seastar::future<int> incr(int i);
+
+seastar::future<> service_loop()
+{
+	return slow_op().then( [] {
+		std::cout << "Done!\n";
+	});
+}
 
 seastar::future<int> fast()
 {
 	return seastar::make_ready_future<int>(3);
 }
 
+seastar::future<> slow_op() 
+{
+	return seastar::sleep(1s);
+}
+
 seastar::future<int> incr(int i)
 {
-	using namespace std::chrono_literals;
 	return seastar::sleep(10ms).then([i] { return i + 1; });
 }
 
 extern seastar::future<> f() 
 {
-	return incr(3).then([] (int val) {
-		std::cout << "Got " << val << std::endl;
+	return seastar::parallel_for_each(boost::irange<unsigned>(0, seastar::smp::count),
+		[] (unsigned c) {
+			// run the given function (service_loop) in a core
+			return seastar::smp::submit_to(c, service_loop);
 	});
 }
 
